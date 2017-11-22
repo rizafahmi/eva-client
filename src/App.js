f08a0682a67d2da4d14b3068fb62f915a19fcc01
Riza Fahmi <rizafahmi@gmail.com>
2017-11-22 17:41:54 +0700

    Panels finish. Eval finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
-  render() {
+  state = {
+    code: '',
+    results: ''
+  }
+  evalCode = async () => {
+    try {
+      const { data } = await axios.post('http://localhost:6543/eva', {
+        code: this.state.code
+      })
+      this.setState({
+        results: data.result
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  handleCodeChange = e => {
+    this.setState({
+      code: e.target.value
+    })
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={styles.container}>
+        <div style={styles.code}>
+          <textarea
+            name='code'
+            placeholder='// Your code here...'
+            onChange={this.handleCodeChange}
+            value={this.state.code}
+            style={styles.panel}
+          />
+          <button onClick={this.evalCode}>Evaluate</button>
+        </div>
+        <div style={styles.result}>
+          <textarea
+            style={styles.panel}
+            name='result'
+            value={this.state.results}
+          />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  code: {
+    flex: 1,
+    backgroundColor: 'mediumaquamarine',
+    height: '100vh'
+  },
+  result: {
+    flex: 1,
+    backgroundColor: '#cacaca'
+  },
+  panel: { background: 'transparent', width: '100%', height: '90vh' }
+}
+
+export default App

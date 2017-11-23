efd194d4780cac49703ae6656b15ebcf316576fc
Riza Fahmi <rizafahmi@gmail.com>
2017-11-23 15:38:12 +0700

    Add evaling state

--- src/App.js
+++ src/App.js
@@ -10,18 +10,35 @@ import './App.css'
 class App extends Component {
   state = {
     code: '',
-    results: ''
+    results: '',
+    doEval: false
+  }
+  componentDidMount () {
+    document.addEventListener('keydown', this.keydownHandler)
   }
   evalCode = async () => {
+    console.log('eval...')
+    this.setState({
+      doEval: true
+    })
     try {
       const { data } = await axios.post('http://localhost:6543/eva', {
         code: this.state.code
       })
       this.setState({
-        results: data.result
+        results: data.result,
+        doEval: false
       })
     } catch (error) {
       console.error(error)
+      this.setState({
+        doEval: false
+      })
+    }
+  }
+  keydownHandler = e => {
+    if (e.keyCode === 13 && e.metaKey) {
+      this.evalCode()
     }
   }
   handleCodeChange = e => {
@@ -30,15 +47,15 @@ class App extends Component {
     })
   }
   onChange = newValue => {
-    console.log('change', newValue)
     this.setState({
       code: newValue
     })
   }
   render () {
+    const textButton = this.state.doEval ? 'Evaluating...' : 'Evaluate now!'
     return (
-      <div style={styles.container}>
-        <div style={styles.code}>
+      <div className='grid' style={styles.container}>
+        <div className='grid__col-6' style={styles.code}>
           <AceEditor
             mode='javascript'
             theme='monokai'
@@ -50,13 +67,22 @@ class App extends Component {
             width='600px'
             fontSize='16px'
           />
-          <button onClick={this.evalCode}>Evaluate</button>
+          <br />
+          <button
+            onClick={this.evalCode}
+            className='btn--default'
+            disabled={this.state.doEval}
+          >
+            {textButton}
+          </button>
         </div>
-        <div style={styles.result}>
+        <div className='grid__col-6' style={styles.result}>
           <textarea
+            className='form__input'
             style={styles.panel}
             name='result'
             value={this.state.results}
+            disabled={true}
           />
         </div>
       </div>
@@ -71,14 +97,12 @@ const styles = {
   },
   code: {
     flex: 1,
-    backgroundColor: 'mediumaquamarine',
     height: '100vh'
   },
   result: {
-    flex: 1,
-    backgroundColor: '#cacaca'
+    flex: 1
   },
-  panel: { background: 'transparent', width: '100%', height: '90vh' }
+  panel: { width: '100%', height: '85vh' }
 }
 
 export default App
